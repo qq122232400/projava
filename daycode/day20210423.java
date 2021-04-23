a8e721a465e4ea66da594d5b2439b3fc1188249e
hrzx201_git122232400 <qq122232400@github.com>
2021-04-23 13:15:39 +0800

    hrzx201_ubuntu_20210423

---
+++ daycode/day20210423.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class day20210423 {
+
+	public static void main(String[] args) {
+		System.out.println("this is java.");
+		//javac,java
+	}
+
+
+}
---
+++ daycode/testoop20210423.java
@@ -0,0 +1,14 @@
+package oop.animal;
+
+public class testoop20210423 {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.sound();
+
+	}
+}
+
+
+class Dog extends Animal {
+
+}
