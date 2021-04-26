3e79a63bc97f5655ba3f79ddec1185c9238568d3
git122232400_vm_ubuntu <qq122232400@github.com>
2021-04-26 10:44:57 -0700

    hrzvmware_ubuntu_202104270144

---
+++ daycode/d21427.java
@@ -0,0 +1,12 @@
+//true code,complemental code,ones-complement code
+public class d21427 {
+	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(0));
+		System.out.println(Integer.toBinaryString(1));
+		System.out.println(Integer.toBinaryString(-1));
+		System.out.println(Integer.toBinaryString(2));
+		System.out.println(Integer.toBinaryString(-2));
+		System.out.println(Integer.toBinaryString(128));
+		System.out.println(Integer.toBinaryString(-128));
+	}
+}
